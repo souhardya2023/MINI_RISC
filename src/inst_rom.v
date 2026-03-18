ea3b1acd0bc9a117fc46e5ae13b27dfc94443373
souhardya2023-inst <sdandapat@kgpian.iitkgp.ac.in>
2026-03-18 21:16:42 +0530

    Fixed inst_rom

--- src/inst_rom.v
+++ src/inst_rom.v
@@ -1,7 +1,26 @@
+// `timescale 1ns / 1ps
+// module inst_rom (
+//     input wire [7:0] addr,
+//     output reg [31:0] dout
+// );
+
+//     reg [31:0] rom [0:255];
+
+//     initial begin
+//         $readmemh("program.mem", rom);
+//     end
+
+//     always @(*) begin
+//         dout = rom[addr];
+//     end
+
+// endmodule
+
 `timescale 1ns / 1ps
 module inst_rom (
-    input wire [7:0] addr,
-    output reg [31:0] dout
+    input wire clka,
+    input wire [7:0] addra,
+    output reg [31:0] douta
 );
 
     reg [31:0] rom [0:255];
@@ -10,8 +29,8 @@ module inst_rom (
         $readmemh("program.mem", rom);
     end
 
-    always @(*) begin
-        dout = rom[addr];
+    always @(posedge clka) begin
+        douta <= rom[addra];
     end
 
 endmodule
